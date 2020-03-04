c1653a878621c7812d1db53190d17405c3af68f5
reifla <alfier@posteo.de>
2020-03-04 18:14:20 +0100

    Added comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,35 @@
 package com.company;
+/*
+creating a permutation of a sorted ArrayList "data_2" in a Array "data_1"
+ */
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
-    static final int lenOfArray = 10;
+
+    static int lenOfArray = 10;
 
     public static void main(String[] args) {
         Random r = new Random();
-        int[] data_1 = new int[lenOfArray];
-        ArrayList<Integer> data = new ArrayList<>();
-        for (int i = 0; i < lenOfArray; i++) {
-            data.add(i);
+        int[] data_1 = new int[lenOfArray];             //contains random permutation afterwards
+        ArrayList<Integer> data_2 = new ArrayList<>();  //contains sorted data
+
+        for (int i = 0; i < lenOfArray; i++) {          //filling up the ArrayList with data
+            data_2.add(i);
         }
-        System.out.println(Arrays.toString(data.toArray()));
+        System.out.println(Arrays.toString(data_2.toArray()));  //checking data in ArrayList
 
         for (int i = 0; i < lenOfArray; i++) {
-            int y = r.nextInt(data.size());
-            data_1[i] = data.get(y);
+            int y = r.nextInt(data_2.size());           //generating rand number 0..size of ArrayList
+            data_1[i] = data_2.get(y);                  //fetching random element of ArrayList and putting it in array
             System.out.println(i + ". -> Random: " + y+" -- Number = " + data_1[i]);
-            data.remove(y);
-            System.out.println(Arrays.toString(data.toArray()));
+            data_2.remove(y);                           //removing element i from sorted ArrayList --> shrinks ArrayL
+            //so one after another element from sorted ArrayList (data_2) is moved to the unsorted Array (data_1)
+            System.out.println(Arrays.toString(data_2.toArray()));
         }
-
-
         System.out.println(Arrays.toString(data_1));
     }
-
 }
 
